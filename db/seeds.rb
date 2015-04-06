123135c404a63d587e23a9d9dc366eb639e4a339
github username <github email address>
2015-04-06 16:47:42 -0400

    added views folders and controllers fors groups, comments and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# NAMES = ["Chris", "Skylar", "Jennifer", "Cyrus", "John"]
+# def emails
+#   emails = []
+#   base_string = "person_0@example.com"
+#   (1..20).each do |number|
+#     emails << base_string.gsub("0", number.to_s)
+#   end
+#   emails
+# end
+
+# EMAILS = emails
+
+# def create_users
+#   User.create(name: NAMES.sample, email: EMAILS[:time], password: "password123")
+# end
+
+# 5.times do 
+#   create_users
+# end
