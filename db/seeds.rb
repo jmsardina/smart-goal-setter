b26877a16bd9fe9d7c34e43320ccf0859a30c035
github username <github email address>
2015-04-06 18:29:32 -0400

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,53 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  # cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+  # Mayor.create(name: 'Emanuel', city: cities.first)
 
-# NAMES = ["Chris", "Skylar", "Jennifer", "Cyrus", "John"]
-# def emails
-#   emails = []
-#   base_string = "person_0@example.com"
-#   (1..20).each do |number|
-#     emails << base_string.gsub("0", number.to_s)
-#   end
-#   emails
-# end
+NAMES = ["Chris", "Skylar", "Jennifer", "Cyrus", "John"]
+GROUP_NAMES = ["Group 1", "Group 2", "Group 3", "Group 4"]
 
-# EMAILS = emails
+def emails
+  emails = []
+  base_string = "person_0@example.com"
+  (1..20).each do |number|
+    emails << base_string.gsub("0", number.to_s)
+  end
+  emails
+end
 
-# def create_users
-#   User.create(name: NAMES.sample, email: EMAILS[:time], password: "password123")
-# end
+EMAILS = emails
 
-# 5.times do 
-#   create_users
-# end
+def create_users
+  index = 0
+  while index < EMAILS.length
+    User.create(name: NAMES.sample, email: EMAILS[index], password: "password123")
+    index += 1
+  end
+end
+
+3.times do 
+  create_users
+end
+
+def create_groups
+  index = 0
+  while index < GROUP_NAMES.length
+    Group.create(name: GROUP_NAMES[index])
+    index += 1
+  end
+end
+
+create_groups
+
+def create_user_groups
+  UserGroup.create([
+    {user_id: 1, group_id: 1},
+    {user_id: 2, group_id: 2},
+    {user_id: 3, group_id: 3}]
+    )
+end
+
+create_user_groups
+
+
