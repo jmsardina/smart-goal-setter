a08aacc70e012bbb4d8f4745aa27d97c8d7fa4a3
github username <github email address>
2015-04-07 14:16:57 -0400

    profile picture is rendered on dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406183012) do
+ActiveRecord::Schema.define(version: 20150407162920) do
 
   create_table "activities", force: :cascade do |t|
     t.text     "description"
     t.integer  "goal_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "period"
+    t.boolean  "status",      default: false
+    t.string   "barrier"
+    t.string   "facilitator"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -43,8 +47,14 @@ ActiveRecord::Schema.define(version: 20150406183012) do
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.boolean  "status",            default: false
+    t.date     "due_date"
+    t.string   "motivation"
+    t.string   "potential_barrier"
+    t.string   "coping_strategy"
+    t.string   "support"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -83,6 +93,10 @@ ActiveRecord::Schema.define(version: 20150406183012) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
