10f6a72433d07ef89fc7603f13620b9636367018
Skylar Panuska <skylar.panuska@gmail.com>
2015-04-07 21:41:15 -0400

    adds group controller, form for creating a group; comments about associations that need work included

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407162920) do
+ActiveRecord::Schema.define(version: 20150407235649) do
 
   create_table "activities", force: :cascade do |t|
     t.text     "description"
@@ -59,8 +59,9 @@ ActiveRecord::Schema.define(version: 20150407162920) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "tags", force: :cascade do |t|
