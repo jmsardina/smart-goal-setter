af65a7999d29a9b1568745dc46dbb57e13fb5cb4
github username <github email address>
2015-04-08 09:00:55 -0400

    enables editing activity description in place from the goal show page

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -3,12 +3,25 @@
   // When checked, the activity info should get crossed out
   // change value to "0"
   // Add class complete
-
-$(function(){
-  $("ul.list").on("change", "input:checkbox", Activity.updateStatus)
-});
-
 function Activity(){
+}
+
+Activity.updateContent = function(e){
+  e.preventDefault();
+
+  var $form = $(this)
+  var href = $form.attr("action");
+
+  $.ajax(href, {
+    "method": "PATCH",
+    "data": $form.serialize(),
+    "success": function(response){
+      var newLabelValue = $("input.edit", $form).val();
+      var $li = $form.parents("li:first");
+      $("label", $li).html(newLabelValue)
+      hideEditInPlace($li);
+    }
+  })
 }
 
 Activity.updateStatus = function(e){
@@ -25,4 +38,28 @@ Activity.updateStatus = function(e){
   })
 }
 
-// To edit activities in place
+Activity.edit = function(e){
+  e.preventDefault();
+  var $li = $(this).parents("li:first");
+  $li.addClass("editing");
+  $("input.edit", $li).focus();
+}
+
+function hideEditInPlace($li){
+  $li.removeClass("editing");
+}
+
+Activity.stopEdit = function(e){
+  e.preventDefault();
+  var $li = $(this).parents("li:first");
+  hideEditInPlace($li);
+
+  $("form.update", $li).trigger("submit")
+}
+
+$(function(){
+  $("ul.list").on("change", "input:checkbox", Activity.updateStatus);
+  $("ul.list").on("submit", "form.update", Activity.updateContent);
+  $("ul.list").on("dblclick", "li label", Activity.edit);
+  $("ul.list").on("blur", "li input.edit", Activity.stopEdit);
+});
