c104ff59046bf9a986ba7066a18120a84a543a43
github username <github email address>
2015-04-15 08:56:13 -0400

    user can only cheer once on any given element

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -2,16 +2,22 @@ class CheersController < ApplicationController
 
   def create
     @cheer = Cheer.new(cheer_params)
-    @cheer.save
-    
-    cheerable_id = cheer_params[:cheerable_id].to_i
-    case cheer_params[:cheerable_type]
-    when "Board"
-      Board.find(cheerable_id).increment!(:cheer_count)
-      redirect_to group_path(Board.find(cheerable_id).group)
-    when "Comment"
-      Comment.find(cheerable_id).increment!(:cheer_count)
-      redirect_to group_path(Board.find(Comment.find(cheerable_id).commentable_id).group)
+
+    if !Cheer.find_by(user_id: current_user.id, cheerable_type: cheer_params[:cheerable_type], cheerable_id: cheer_params[:cheerable_id])
+      @cheer.save
+  
+      cheerable_id = cheer_params[:cheerable_id].to_i
+
+      case cheer_params[:cheerable_type]
+      when "Board"
+        Board.find(cheerable_id).increment!(:cheer_count)
+        redirect_to group_path(Board.find(cheerable_id).group)
+      when "Comment"
+        Comment.find(cheerable_id).increment!(:cheer_count)
+        redirect_to group_path(Board.find(Comment.find(cheerable_id).commentable_id).group)
+      end
+    else
+      redirect_to root_path
     end
   end
 
