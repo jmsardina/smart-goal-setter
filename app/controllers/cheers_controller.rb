97f6f0577c5ab33ac237a8e3e5d5a68cd7b65da2
github username <github email address>
2015-04-15 08:33:17 -0400

    enabled users to cheer on comments

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -3,14 +3,14 @@ class CheersController < ApplicationController
   def create
     @cheer = Cheer.new(cheer_params)
     @cheer.save
-binding.pry
+    
     cheerable_id = cheer_params[:cheerable_id].to_i
     case cheer_params[:cheerable_type]
     when "Board"
-      Board.find(cheerable_id).increment!(:cheer_counter)
+      Board.find(cheerable_id).increment!(:cheer_count)
       redirect_to group_path(Board.find(cheerable_id).group)
     when "Comment"
-      Comment.find(cheerable_id).increment!(:cheer_counter)
+      Comment.find(cheerable_id).increment!(:cheer_count)
       redirect_to group_path(Board.find(Comment.find(cheerable_id).commentable_id).group)
     end
   end
