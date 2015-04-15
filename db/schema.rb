b202df060b90ae97dffa58d99942deceb544d2f5
Skylar Panuska <skylar.panuska@gmail.com>
2015-04-15 12:23:43 -0400

    adds theme formatting to new goal view - description, name, and tag fields

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,16 @@ ActiveRecord::Schema.define(version: 20150415130524) do
   add_index "feeds", ["trackable_id"], name: "index_feeds_on_trackable_id"
   add_index "feeds", ["user_id"], name: "index_feeds_on_user_id"
 
+  create_table "goal_tags", force: :cascade do |t|
+    t.integer  "goal_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "goal_tags", ["goal_id"], name: "index_goal_tags_on_goal_id"
+  add_index "goal_tags", ["tag_id"], name: "index_goal_tags_on_tag_id"
+
   create_table "goals", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -90,10 +100,10 @@ ActiveRecord::Schema.define(version: 20150415130524) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.integer  "creator_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "description"
+    t.integer  "creator_id"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -103,21 +113,22 @@ ActiveRecord::Schema.define(version: 20150415130524) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
-    t.string   "taggable_type"
-    t.integer  "taggable_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  create_table "user_groups", force: :cascade do |t|
-    t.integer  "group_id"
-    t.integer  "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_groups", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_groups", ["group_id"], name: "index_user_groups_on_group_id"
+  add_index "user_groups", ["user_id"], name: "index_user_groups_on_user_id"
+
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
