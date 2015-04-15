70aaae251006e560cfa6c72a385a8e47414944aa
cyzanfar <cghazanfar10@gmail.com>
2015-04-15 00:28:09 -0400

    tracking board creation

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,8 @@ class BoardsController < ApplicationController
   def create
     @board = Board.new(name: params[:board][:name], group_id: params[:group_id])
     if @board.save
+      # binding.pry
+      track_feed(@board)
       redirect_to group_path(@board.group)
     end
   end
