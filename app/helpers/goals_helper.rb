f4e722aa61a499a9a2982fbe8a1f0bead86d33e8
cyzanfar <cghazanfar10@gmail.com>
2015-04-15 21:31:48 -0400

    add percentage activity completion

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,7 +1,23 @@
 module GoalsHelper
   def li_for_goal(goal)
     content_tag :li, :"data-goal-id" => goal.id, class: (goal.complete? ? "completed" : "") do
-      yield 
+      yield
     end
+  end
+
+  def total_activity_occurence
+    occurence = 0
+    self.activities.each do |activity|
+      occurence += activity.number_occurences
+    end
+    occurence
+  end
+
+  def total_remaining_occurence
+    remaining = 0
+    self.activities.each do |activity|
+      remaining += activity.occurences
+    end
+    remaining
   end
 end
