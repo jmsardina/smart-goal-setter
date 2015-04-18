0783fc519db0f9a31767b5f1c5f688737ef7329b
github username <github email address>
2015-04-18 18:04:12 -0400

    removed one board from group show page

--- app/assets/javascripts/goal.js
+++ app/assets/javascripts/goal.js
@@ -2,6 +2,7 @@ function Goal(){
 }
 
 Goal.updateStatus = function(e){
+  debugger
   var $checkbox = $(this);
   var $form = $(this).parents("form:first");
   var href = $form.attr("action");
@@ -42,7 +43,8 @@ Invitation.declineRequest = function(){
 }
 
 $(function(){
-  $("ul.list-goals").on("change", "input:checkbox", Goal.updateStatus);
+  $("ul.list").on("change", "input#goal_status", Goal.updateStatus);
+  // $("ul.list-goals").on("change", "input:checkbox", Goal.updateStatus);
   $("ul.list-goals").on("click", "button.destroy", deleteGoal);
   $("div.container").on("click", "button.destroy", Invitation.declineRequest)
 });
