c4b04ce38aa040e7f8448407b2147a29d82960f9
github username <github email address>
2015-04-25 12:54:10 -0400

    adjusts remaining_for_period when user modifies frequency

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -44,20 +44,29 @@ class ActivitiesController < ApplicationController
 	def update
 		@goal = Goal.find(params[:goal_id])
 		@activity = @goal.activities.find(params[:id])
-		@activity.update(activity_params)
-		@activity.upcoming_deadline = @activity.upcoming_due_dates[0]
-		for i in 1..@activity.upcoming_due_dates.size-1
-			Activity.new(activity_params) do |a|
-				a.goal = @goal
-				a.save #object must be persisted in order to find upcoming_due_dates
-				a.occurences = @activity.occurences
-				a.upcoming_deadline = @activity.upcoming_due_dates[i]
-				a.save
+		if activity_params[:status]
+			@activity.add_point_and_decrement_occurences
+			@activity.update(activity_params)
+		else
+			old_frequency = @activity.frequency
+			currently_complete = @activity.frequency - @activity.remaining_for_period
+			Activity.where(goal_id: @goal.id, description: @activity.description).each do |activity|
+				activity.update(activity_params)
+			end
+			@activity.update(activity_params)
+			@activity.occurences = @activity.number_occurences
+			@activity.upcoming_deadline = @activity.upcoming_due_dates[0]
+			@activity.remaining_for_period = @activity.frequency - @activity.activity_points
+			for i in 1..@activity.upcoming_due_dates.size-1
+				Activity.where(goal_id: @goal.id, description: @activity.description) do |a|
+					a.occurences = @activity.occurences
+					a.upcoming_deadline = @activity.upcoming_due_dates[i]
+					a.save
+				end
 			end
 		end
+
 		@activity.save
-		track_feed(@activity)
-		@activity.add_point_and_decrement_occurences
 		render nothing: true, status: :ok
 	end
 
